d2f1eaeb5454d51cc82e86b7a7a7902c60829b17
luis jose <luissjosse@gmai.com>
2014-09-05 10:08:48 -0400

    comentario

--- SumaTesting/src/com/freesoft/test/TestSuma.java
+++ SumaTesting/src/com/freesoft/test/TestSuma.java
@@ -7,6 +7,7 @@ import junit.framework.TestCase;
 public class TestSuma extends TestCase {
 
 	public static void main(String[] args) {
+		// Este sera el 3er commit
 		Suma suma = new Suma();
 		double a = 1.0;
 		double b = 1.2;
